0f350257ee2d07b981cc19c6cf782aa73e7ce556
Braxton Fair <hello@taxborn.com>
2023-02-15 08:40:45 -0600

    Remove dead code from plotting

--- plot.py
+++ plot.py
@@ -1,33 +1,7 @@
 import random
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
-from collections import deque
-
-SEQ_LEN = 60
 from matplotlib import pyplot as plt
-from matplotlib import dates as mpl_dates
-
-
-def preprocess(df):
-    for col in df.columns:
-        if col != "ID":
-            df[col] = preprocessing.normalize([i[:-1] for i in df.values], axis=1)
-    df = df.values
-    sequential_data = []
-    prev_data = deque(maxlen=SEQ_LEN)
-    for i in df:
-        prev_data.append([n for n in i[:-1]])
-        if len(prev_data) == SEQ_LEN:
-            sequential_data.append([np.array(prev_data), i[-1]])
-    random.shuffle(sequential_data)
-    X = []
-    y = []
-    for seq, target in sequential_data:
-        X.append(seq)
-        y.append(target)
-    return np.array(X), np.array(y)
-
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
@@ -50,5 +24,4 @@ if __name__ == '__main__':
     plt.plot(x, y, color='#67F8A0')
     plt.show()
 
-    # test_X, test_y = preprocess(test_df)
     print("Done with test")
