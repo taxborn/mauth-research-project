f2eadcf5e29a3cc64ffe0f248f8e6a09763f5099
Braxton Fair <hello@taxborn.com>
2023-03-17 11:06:40 -0500

    Start Model scaffolding

---
+++ models.py
@@ -0,0 +1,26 @@
+def knn():
+    """
+    K-Nearest Neighbor Model
+    :return:
+    """
+    pass
+
+def svm():
+    """
+    Support Vector Machine / C-Support Vector Classification Model
+    :return:
+    """
+    pass
+
+def dt():
+    """
+    Decision Tree Model
+    :return:
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
