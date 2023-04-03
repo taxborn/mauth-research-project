efc7cab2d3be762e496d926123f9ba18e7f8b79e
Braxton Fair <hello@taxborn.com>
2023-04-03 15:52:42 -0500

    start preprocessing rework

--- preprocessing.py
+++ preprocessing.py
@@ -1,38 +1,11 @@
+import time
 import numpy as np
 import pandas as pd
 from collections import deque
 import os.path
 
-
-def data_to_df(file_path):
-    df = pd.read_csv(file_path)
-    # Insert columns and run calculations
-    df.insert(len(df.columns) - 1, "X_Speed", 0)
-    df.insert(len(df.columns) - 1, "Y_Speed", 0)
-    df.insert(len(df.columns) - 1, "Speed", 0)
-    df.insert(len(df.columns) - 1, "X_Acceleration", 0)
-    df.insert(len(df.columns) - 1, "Y_Acceleration", 0)
-    df.insert(len(df.columns) - 1, "Acceleration", 0)
-    df.insert(len(df.columns) - 1, "Jerk", 0)
-    df.insert(len(df.columns) - 1, "Ang_V", 0)
-    df.insert(len(df.columns) - 1, "Path_Tangent", 0)
-    df.insert(len(df.columns) - 1, "Direction", 0)
-
-    df = df.loc[(df["X"].shift() != df["X"]) | (df["Y"].shift() != df["Y"])]  # Remove repeat data
-    df['X_Speed'] = (df.X - df.X.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Y_Speed'] = (df.Y - df.Y.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Speed'] = np.sqrt((df.X_Speed ** 2) + (df.Y_Speed ** 2))
-    df['X_Acceleration'] = (df.X_Speed - df.X_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Y_Acceleration'] = (df.Y_Speed - df.Y_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Acceleration'] = (df.Speed - df.Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Jerk'] = (df.Acceleration - df.Acceleration.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    df['Path_Tangent'] = np.arctan2((df.Y - df.Y.shift(1)), (df.X - df.X.shift(1)))
-    df['Ang_V'] = (df.Path_Tangent - df.Path_Tangent.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-    # Fill empty data with 0
-    df.fillna(0)
-    print(f"Size: {df.size} \nShape {df.shape} \nColumn Names: {df.columns}")
-    df = sequence_maker(df)
-    return df
+import constants
+import utils
 
 
 def sequence_maker(df, sequence_length=64):
@@ -57,21 +30,6 @@ def sequence_maker(df, sequence_length=64):
             if button_press_time == 0:
                 button_press_time = press_avg
 
-            mean_x_speed = temp[1:, 5].mean()
-            std_x_speed = temp[1:, 5].std()
-            min_x_speed = temp[1:, 5].min()
-            max_x_speed = temp[1:, 5].max()
-
-            mean_y_speed = temp[1:, 6].mean()
-            std_y_speed = temp[1:, 6].std()
-            min_y_speed = temp[1:, 6].min()
-            max_y_speed = temp[1:, 6].max()
-
-            mean_speed = temp[1:, 7].mean()
-            std_speed = temp[1:, 7].std()
-            min_speed = temp[1:, 7].min()
-            max_speed = temp[1:, 7].max()
-
             # Calculate mean speeds over distance
             dx = np.diff(temp[1:, 2])
             dy = np.diff(temp[1:, 3])
@@ -84,21 +42,6 @@ def sequence_maker(df, sequence_length=64):
             min_speed_over_dist = np.min(speed_over_dist)
             max_speed_over_dist = np.max(speed_over_dist)
 
-            mean_x_acc = temp[1:, 8].mean()
-            std_x_acc = temp[1:, 8].std()
-            min_x_acc = temp[1:, 8].min()
-            max_x_acc = temp[1:, 8].max()
-
-            mean_y_acc = temp[1:, 9].mean()
-            std_y_acc = temp[1:, 9].std()
-            min_y_acc = temp[1:, 9].min()
-            max_y_acc = temp[1:, 9].max()
-
-            mean_acc = temp[1:, 10].mean()
-            std_acc = temp[1:, 10].std()
-            min_acc = temp[1:, 10].min()
-            max_acc = temp[1:, 10].max()
-
             acceleration = np.divide(np.diff(speed_over_dist), time[:-1])
 
             mean_acceleration_over_dist = np.mean(acceleration)
@@ -106,20 +49,7 @@ def sequence_maker(df, sequence_length=64):
             min_acceleration_over_dist = np.min(acceleration)
             max_acceleration_over_dist = np.max(acceleration)
 
-            mean_jerk = temp[1:, 11].mean()
-            std_jerk = temp[1:, 11].std()
-            min_jerk = temp[1:, 11].min()
-            max_jerk = temp[1:, 11].max()
 
-            mean_ang = temp[1:, 12].mean()
-            std_ang = temp[1:, 12].std()
-            min_ang = temp[1:, 12].min()
-            max_ang = temp[1:, 12].max()
-
-            mean_tan = temp[1:, 13].mean()
-            std_tan = temp[1:, 13].std()
-            min_tan = temp[1:, 13].min()
-            max_tan = temp[1:, 13].max()
 
             elapsed_time = temp[-1, 0] - temp[0, 0]
             # Initialize variables and data structures.
@@ -169,7 +99,7 @@ def sequence_maker(df, sequence_length=64):
             max_curve = np.max(curve_list)
 
             sum_of_angles = np.sum(temp[1:, 12])
-            #sharp_angles = np.sum(abs(temp[1:, 12]) < .0005)
+            # sharp_angles = np.sum(abs(temp[1:, 12]) < .0005)
 
             for jj in [[mean_x_speed, mean_y_speed, mean_speed, mean_x_acc, mean_y_acc, mean_acc, mean_jerk, mean_ang,
                         mean_curve, mean_tan,
@@ -180,7 +110,9 @@ def sequence_maker(df, sequence_length=64):
                         max_x_speed, max_y_speed, max_speed, max_x_acc, max_y_acc, max_acc, max_ang, max_jerk,
                         max_curve, max_tan,
                         elapsed_time, sum_of_angles, accTimeatBeg, traj_length, numCritPoints, button_press_time,
-                        mean_speed_over_dist, std_speed_over_dist, min_speed_over_dist, max_speed_over_dist, mean_acceleration_over_dist, std_acceleration_over_dist, max_acceleration_over_dist, min_acceleration_over_dist]]:
+                        mean_speed_over_dist, std_speed_over_dist, min_speed_over_dist, max_speed_over_dist,
+                        mean_acceleration_over_dist, std_acceleration_over_dist, max_acceleration_over_dist,
+                        min_acceleration_over_dist]]:
                 sequential_data.append(
                     jj)  # Prev_data now contains SEQ_LEN amount of samples and can be appended as one batch of 60 for RNN
         count += 1
@@ -202,7 +134,8 @@ def sequence_maker(df, sequence_length=64):
                                'elapsed_time', 'sum_of_angles', 'accTimeatBeg', 'traj_length', 'numCritPoints',
                                'button_press_time', 'mean_speed_over_dist', 'std_speed_over_dist',
                                'min_speed_over_dist', 'max_speed_over_dist', 'mean_acceleration_over_dist',
-                               'std_acceleration_over_dist', 'max_acceleration_over_dist', 'min_acceleration_over_dist'])
+                               'std_acceleration_over_dist', 'max_acceleration_over_dist',
+                               'min_acceleration_over_dist'])
     df.insert(0, 'ID', ID)
     df.fillna(0)
     print(f"Head: {df.head()} \nSize: {df.size} \nShape {df.shape} \nColumn Names: {df.columns}")
@@ -210,7 +143,201 @@ def sequence_maker(df, sequence_length=64):
     return df
 
 
+def subject_to_dataframe(path: str, remove_duplicates: bool = True) -> pd.DataFrame:
+    print(f"Converting {path} into a mAuth subject dataframe...")
+    # Check if it is a valid path
+    if not os.path.isfile(path):
+        raise FileNotFoundError(f"The subject path {path} was not found.")
+
+    # Create dataframe
+    df = pd.read_csv(path)
+
+    # Insert columns needed for feature calculations
+    df.insert(len(df.columns) - 1, "X_Speed", 0)
+    df.insert(len(df.columns) - 1, "Y_Speed", 0)
+    df.insert(len(df.columns) - 1, "Speed", 0)
+    df.insert(len(df.columns) - 1, "X_Acceleration", 0)
+    df.insert(len(df.columns) - 1, "Y_Acceleration", 0)
+    df.insert(len(df.columns) - 1, "Acceleration", 0)
+    df.insert(len(df.columns) - 1, "Jerk", 0)
+    df.insert(len(df.columns) - 1, "Path_Tangent", 0)
+    df.insert(len(df.columns) - 1, "Ang_V", 0)
+    # df.insert(len(df.columns) - 1, "Direction", 0)
+
+    # Remove rows where the next row has the exact same X and Y value, for example, if we had:
+    # BEFORE:
+    #   .. | X | Y | ...
+    #   .. | 2 | 5 | ...
+    #   .. | 3 | 6 | ...
+    #   .. | 3 | 6 | ...
+    #   .. | 3 | 5 | ...
+    # Then after this line the dataframe would become:
+    # AFTER:
+    #   .. | X | Y | ...
+    #   .. | 2 | 5 | ...
+    #   .. | 3 | 6 | ...
+    #   .. | 3 | 5 | ...
+    # Removing the duplicate (3, 6).
+    if remove_duplicates:
+        df = df.loc[(df["X"].shift() != df["X"]) | (df["Y"].shift() != df["Y"])]
+        print(" > removed duplicate rows")
+
+    # Now populate the inserted columns
+    print(" > Populating new columns used for features")
+    start = time.time()
+    df['X_Speed'] = (df.X - df.X.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Y_Speed'] = (df.Y - df.Y.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Speed'] = np.sqrt((df.X_Speed ** 2) + (df.Y_Speed ** 2))
+    df['X_Acceleration'] = (df.X_Speed - df.X_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Y_Acceleration'] = (df.Y_Speed - df.Y_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Acceleration'] = (df.Speed - df.Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Jerk'] = (df.Acceleration - df.Acceleration.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    df['Path_Tangent'] = np.arctan2((df.Y - df.Y.shift(1)), (df.X - df.X.shift(1)))
+    df['Ang_V'] = (df.Path_Tangent - df.Path_Tangent.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    took = round(time.time() - start, constants.NUM_ROUNDING)
+    print(f" > Done! ({took =}s)")
+
+    return df
+
+
+def split(dataframe: pd.DataFrame, n: int) -> list[pd.DataFrame]:
+    sequences = []
+    ID = int(dataframe.iloc[1]['ID'])
+
+    print(f"Splitting subject {ID} into sequences ({n = }).")
+
+    start = time.time()
+    for idx in range(0, dataframe.shape[0], n):
+        sequence = dataframe.iloc[idx:idx + n]
+        sequences.append(sequence)
+
+    took = round(time.time() - start, constants.NUM_ROUNDING)
+    print(f" > {took = }s")
+
+    return sequences
+
+def process(sequences: list[pd.DataFrame]) -> pd.DataFrame:
+    dfs = []
+    start_time = time.time()
+    for count, sequence in enumerate(sequences):
+        sql_data = []
+        """ Calculate start/end time and how long the sequence lasts """
+        sequence_start, sequence_end = sequence['Timestamp'][~sequence['Timestamp'].isna()].values[[0, -1]]
+        elapsed = sequence_end - sequence_end
+
+        """ Calculate speeds """
+        mean_x_speed = sequence['X_Speed'].mean()
+        std_x_speed = sequence['X_Speed'].std()
+        min_x_speed = sequence['X_Speed'].min()
+        max_x_speed = sequence['X_Speed'].max()
+
+        mean_y_speed = sequence['Y_Speed'].mean()
+        std_y_speed = sequence['Y_Speed'].std()
+        min_y_speed = sequence['Y_Speed'].min()
+        max_y_speed = sequence['Y_Speed'].max()
+
+        mean_speed = sequence['Speed'].mean()
+        std_speed = sequence['Speed'].std()
+        min_speed = sequence['Speed'].min()
+        max_speed = sequence['Speed'].max()
+
+        """ Calculate accelerations """
+        mean_x_acc = sequence['X_Acceleration'].mean()
+        std_x_acc = sequence['X_Acceleration'].std()
+        min_x_acc = sequence['X_Acceleration'].min()
+        max_x_acc = sequence['X_Acceleration'].max()
+
+        mean_y_acc = sequence['Y_Acceleration'].mean()
+        std_y_acc = sequence['Y_Acceleration'].std()
+        min_y_acc = sequence['Y_Acceleration'].min()
+        max_y_acc = sequence['Y_Acceleration'].max()
+
+        mean_acc = sequence['Y_Acceleration'].mean()
+        std_acc = sequence['Y_Acceleration'].std()
+        min_acc = sequence['Y_Acceleration'].min()
+        max_acc = sequence['Y_Acceleration'].max()
+
+        """ Calculate Jerk """
+        mean_jerk = sequence['Jerk'].mean()
+        std_jerk = sequence['Jerk'].std()
+        min_jerk = sequence['Jerk'].min()
+        max_jerk = sequence['Jerk'].max()
+
+        """ Calculate angular velocity """
+        mean_ang = sequence['Ang_V'].mean()
+        std_ang = sequence['Ang_V'].std()
+        min_ang = sequence['Ang_V'].min()
+        max_ang = sequence['Ang_V'].max()
+
+        """ Calculate Path Tangents """
+        mean_tan = sequence['Path_Tangent'].mean()
+        std_tan = sequence['Path_Tangent'].std()
+        min_tan = sequence['Path_Tangent'].min()
+        max_tan = sequence['Path_Tangent'].max()
+
+        # Initialize variables and data structures.
+        curve_list = []  # a list to store the curvature values for each segment of the trajectory
+        traj_length = 0  # the total length of the trajectory
+        accTimeatBeg = 0  # the accumulated time spent in acceleration at the beginning of the trajectory
+        numCritPoints = 0  # the number of critical points (where the curvature is very low)
+        path = list()  # a list to store the distances traveled for each segment of the trajectory
+        flag = True  # a flag to indicate whether the trajectory is in the acceleration phase
+
+        for idx in range(1, 8):
+            x_start, x_end = sequence['X'][~sequence['X'].isna()].values[[idx, idx-1]]
+            y_start, y_end = sequence['Y'][~sequence['Y'].isna()].values[[idx, idx-1]]
+            traj_length += np.sqrt((x_end - x_start) ** 2 + (y_end - y_start) ** 2)
+            path.append(traj_length)
+
+            sequence_start, sequence_end = sequence['Timestamp'][~sequence['Timestamp'].isna()].values[[idx, idx-1]]
+            dt = sequence_end - sequence_end
+            sequence_start, sequence_end = sequence['Speed'][~sequence['Speed'].isna()].values[[idx, idx-1]]
+            dv = sequence_end - sequence_end
+
+            if dv > 0 and flag:
+                accTimeatBeg += dt
+            else:
+                flag = False
+
+        for jj in [[mean_x_speed, mean_y_speed, mean_speed, mean_x_acc, mean_y_acc, mean_acc, mean_jerk, mean_ang,
+                    mean_curve, mean_tan,
+                    std_x_speed, std_y_speed, std_speed, std_x_acc, std_y_acc, std_acc, std_ang, std_jerk,
+                    std_curve, std_tan, min_tan,
+                    min_x_speed, min_y_speed, min_speed, min_x_acc, min_y_acc, min_acc, min_ang, min_jerk,
+                    min_curve,
+                    max_x_speed, max_y_speed, max_speed, max_x_acc, max_y_acc, max_acc, max_ang, max_jerk,
+                    max_curve, max_tan,
+                    elapsed_time, sum_of_angles, accTimeatBeg, traj_length, numCritPoints, button_press_time,
+                    mean_speed_over_dist, std_speed_over_dist, min_speed_over_dist, max_speed_over_dist,
+                    mean_acceleration_over_dist, std_acceleration_over_dist, max_acceleration_over_dist,
+                    min_acceleration_over_dist]]:
+
+    end = time.time()
+    took = round(end - start_time, constants.NUM_ROUNDING)
+    print(f" > Done! ({took = }s)")
+
+
+
 if __name__ == "__main__":
-    for i in range(15):
-        subj_df = data_to_df(f"raw_data/user_{i}_data.csv")
-        print('done')
+    n = 8
+
+    featured_subjects = []
+
+    preprocessing_start = time.time()
+    # Create each individual subjects feature CSV
+    for subject in range(constants.SUBJECTS):
+        # Translate raw data into a pre-populated dataframe
+        prefeatured_dataframe = subject_to_dataframe(f"raw_data/user_{subject}_data.csv")
+
+        # Split the dataframe into sequences of length n.
+        sequences = split(prefeatured_dataframe, n)
+        print(f"{len(sequences)} sequences of {n} created. (raw shape = {prefeatured_dataframe.shape})")
+
+        # Recalculate entire feature set
+        featured_dataframe = process(sequences)
+
+    # utils.join_dataframes_to_csv(featured_subjects, "synth_data/user_all_feature_data.csv")
+
+    preprocessing_end = time.time()
+    took = round(preprocessing_end - preprocessing_start, constants.NUM_ROUNDING)
+    print(f"Preprocessing complete. ({took = }s)")
